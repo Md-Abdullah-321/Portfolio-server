35720ea61d9f17c8d6bae22ffcdf3a0d3dcf3fd5
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2024-04-20 18:10:51 +0600

    optimized handleGetUserData

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,68 +1,58 @@
 const User = require("../model/userSchema");
 const { successResponse } = require("./responseControllers");
 
-
-
 const handlePostUserData = async (req, res, next) => {
     try {
-        const {username, email, password, phoneNumber, profilePicture, bio, resume} = req.body;
+        const { username, email, password, phoneNumber, profilePicture, bio, resume } = req.body;
 
-        if(!username || !email || !password || !phoneNumber || !profilePicture || !bio || !resume){
-            return res.status(400).json({"message": "Please fill all the input fields"});
+        if (!username || !email || !password || !phoneNumber || !profilePicture || !bio || !resume) {
+            return res.status(400).json({ "message": "Please fill all the input fields" });
         }
 
-        await User.create({username, email, phoneNumber, password, profilePicture, bio, resume});
+        await User.create({ username, email, phoneNumber, password, profilePicture, bio, resume });
 
-        res.status(200).json({"message": "User Data inserted successfully."})
+        res.status(200).json({ "message": "User Data inserted successfully." });
     } catch (error) {
         next(error);
     }
-}
-
-
-
+};
 
 const handleGetUserData = async (req, res, next) => {
     try {
-        const userData = await User.find({})
+        const userData = await User.find({}).limit(1);
 
         return successResponse(res, {
             statusCode: 200,
             message: "User data fetched successfully",
             payload: userData[0],
-        })
+        });
     } catch (error) {
         next(error);
     }
-}
-
-
+};
 
 const handleUpdateUserData = async (req, res, next) => {
     try {
-        const {username, email, password, phoneNumber, profilePicture, bio, resume} = req.body;
+        const { username, email, password, phoneNumber, profilePicture, bio, resume } = req.body;
 
-        const update = {username, email, password, phoneNumber, profilePicture, bio, resume};
-        const userInfo = await User.find({})
-        const id = userInfo[0]._id;
-        const updatedUserInfo = await User.findOneAndUpdate(id, update, {
+        const update = { username, email, password, phoneNumber, profilePicture, bio, resume };
+
+        const updatedUserInfo = await User.findOneAndUpdate({}, update, {
             new: true
-          });
+        });
 
         return successResponse(res, {
             statusCode: 200,
             message: "User data updated successfully",
             payload: updatedUserInfo,
-        })
+        });
     } catch (error) {
         next(error);
     }
-}
-
-
+};
 
 module.exports = {
     handlePostUserData,
     handleGetUserData,
     handleUpdateUserData,
-}
+};
