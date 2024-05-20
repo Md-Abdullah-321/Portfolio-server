8881ba66fc8a66ae224831f6d8a5cc82d21678f8
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2024-05-20 14:31:39 +0600

    added conditon for blank input field

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,10 +5,18 @@ const { successResponse, errorResponse } = require("./responseControllers");
 const handleUserLogin = async (req, res, next) => {
     try {
        const {email, password} = req.body;
+       if(!email || !password){
+        return  errorResponse(res, {
+            statusCode: 204,
+            message: "Plese fill all the input field."
+        })
+   }
        const user = await Auth.find({});
     //    const salt = bcrypt.genSaltSync(10);
     //    const hashedPassword = bcrypt.hashSync(password, salt);
     //    await Auth.create({email, password:hashedPassword});
+       
+       
        const isMatched = bcrypt.compareSync(password, user[0]["password"]);
        if(isMatched){
         return successResponse(res, {
@@ -16,7 +24,7 @@ const handleUserLogin = async (req, res, next) => {
             message: "Logged in successfully"
         })
        }else{
-        errorResponse(res, {
+        return errorResponse(res, {
             statusCode: 401,
             message: "Wrong password, please try again"
         })
