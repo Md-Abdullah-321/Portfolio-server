8a224c0f1faf6f7c2911c8361df26b214f63865a
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2024-07-09 02:32:41 +0600

    updated login apis

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,46 +1,59 @@
 const bcrypt = require("bcryptjs");
 const Auth = require("../model/authSchema");
 const { successResponse, errorResponse } = require("./responseControllers");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const handleUserLogin = async (req, res, next) => {
     try {
-       const {email, password} = req.body;
-       if(!email || !password){
-            return  errorResponse(res, {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return errorResponse(res, {
                 statusCode: 204,
-                message: "Plese fill all the input field."
-            })
+                message: "Please fill all the input fields."
+            });
         }
-        const user = await Auth.find({});
-       
-        const isMatched = bcrypt.compareSync(password, user[0]["password"]);
-        if(isMatched){
-        const token = jwt.sign({ username }, process.env.SECRET_KEY , {
-            expiresIn: '7d' 
-        });
 
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure: true,
-        });
-        return successResponse(res, {
-            statusCode: 200,
-            message: "Logged in successfully"
-        })
-       }else{
-        return errorResponse(res, {
-            statusCode: 401,
-            message: "Wrong password, please try again"
-        })
-       }
-      } catch (error) {
+        
+        const user = await Auth.findOne({ email });
+        if (!user) {
+            return errorResponse(res, {
+                statusCode: 401,
+                message: "User not found or credentials are invalid."
+            });
+        }
+
+        // Compare passwords
+        const isMatched = bcrypt.compareSync(password, user.password);
+        if (isMatched) {
+            // Generate JWT token
+            const token = jwt.sign({ email: user.email }, process.env.SECRET_KEY, {
+                expiresIn: '7d'
+            });
+
+            // Set cookie with JWT token
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true, 
+                sameSite: 'none' 
+            });
+
+            return successResponse(res, {
+                statusCode: 200,
+                message: "Logged in successfully"
+            });
+        } else {
+            return errorResponse(res, {
+                statusCode: 401,
+                message: "Wrong password, please try again"
+            });
+        }
+    } catch (error) {
         console.error(error);
-        res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({ message: "Internal server error" });
     }
-}
-
+};
 
 module.exports = {
     handleUserLogin,
-}
+};
