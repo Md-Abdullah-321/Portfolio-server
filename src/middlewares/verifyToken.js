5f37736cfcf95a72224dd4177418f11626a6f23b
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2024-08-02 16:26:15 +0600

    sending token to frontEnd

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = process.env;
 
 const verifyToken = (req, res, next) => {
-  const token = req.cookies.token;
+  const token = req.headers.authorization && req.headers.authorization.split(' ')[1]; 
 
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized: No token provided' });
